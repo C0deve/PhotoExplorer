41f01c7061e338ef41ac5c62fc1b6fe44198baee
CODEVE <codevesoft@proton.me>
2025-01-31 01:35:42 +0100

    Initial commit

---
+++ FileCopyFinder/Core.fs
@@ -0,0 +1,37 @@
+﻿namespace FileCopyFinder
+
+module Core =
+    open System.IO
+
+    let getFilesGroupByName (path: string) : Map<string,string list> =
+        Directory.EnumerateFileSystemEntries(path, "*", SearchOption.AllDirectories)
+        |> Seq.map (fun x ->
+            {|  Name = Path.GetFileNameWithoutExtension x
+                Path = x
+            |})
+        |> Seq.sortBy  _.Name
+        |> Seq.groupBy  _.Name // Group by the name before the extension
+        |> Seq.map (fun (key, group) -> 
+            let pathList = group |> Seq.map _.Path |> Seq.toList
+            key, pathList )
+        |> Map.ofSeq
+    
+    let findCopy (inputFiles: Map<string, string list>) =
+        let mapList = Map.toList inputFiles 
+        ([], mapList)
+        ||> List.fold (fun state (currentKey, currentFiles) ->
+            match state with
+            | [] -> [ (currentKey, currentFiles) ]
+            | [ (key, files) ] when currentKey.StartsWith key -> [ (key, currentFiles @ files) ]
+            | (key, files) :: tail when currentKey.StartsWith key -> (key, currentFiles @ files) :: tail
+            | _ -> (currentKey, currentFiles) :: state)
+        |> Map.ofList
+            
+    let print (groupedFiles:Map<string,string list>) =
+        groupedFiles
+        |> Map.iter (fun key files ->
+            printfn $"%s{key} {files.Length} files"
+            files
+            |> List.iter (fun path ->
+                let parent = Path.GetDirectoryName path
+                printfn $"  {path}  {parent}"))
---
+++ FileCopyFinder/Draft.fsx
@@ -0,0 +1,9 @@
+﻿#load "Core.fs"
+
+open System.IO
+
+let files = FileCopyFinder.Core.getFilesGroupByName "D:\\Veranda\\Tmp"
+
+files |> FileCopyFinder.Core.findCopy |> FileCopyFinder.Core.print
+
+let drives = DriveInfo.GetDrives()
